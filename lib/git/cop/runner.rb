4c2759dc69556f56b1668903b5ce2942f68bcf8c
Brooke Kuhlmann <brooke@alchemists.io>
2017-06-10 10:09:22 -0600

    Refactored runner implementation.
    - Minor cleanup related to loading of commits and cops.
    - Improved method names to be a bit more self-describing.

--- lib/git/cop/runner.rb
+++ lib/git/cop/runner.rb
@@ -6,30 +6,26 @@ module Git
       def initialize configuration:, reporter: Reporter.new
         @configuration = configuration
         @reporter = reporter
-        @branch = Branch.new
+        @commits = Branch.new.shas.map { |sha| Git::Cop::Commit.new sha: sha }
       end
 
       def run
-        commits.each { |commit| report commit }
+        commits.each { |commit| check commit }
         reporter
       end
 
       private
 
-      attr_reader :configuration, :reporter, :branch
+      attr_reader :configuration, :reporter, :commits
 
-      def commits
-        branch.shas.map { |sha| Git::Cop::Commit.new sha: sha }
-      end
-
-      def initialize_cop id, commit, settings
+      def load_cop id, commit, settings
         klass = Styles::Abstract.descendants.find { |descendant| descendant.id == id }
         fail(StandardError, "Invalid cop: #{id}. See docs for supported cops.") unless klass
         klass.new commit: commit, settings: settings
       end
 
-      def report commit
-        cops = configuration.map { |id, settings| initialize_cop id, commit, settings }
+      def check commit
+        cops = configuration.map { |id, settings| load_cop id, commit, settings }
         cops.select(&:enabled?).map { |cop| reporter.add cop }
       end
     end
