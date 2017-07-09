57e413a2a8ae278669744998d66274a310cfc337
Brooke Kuhlmann <brooke@alchemists.io>
2017-07-09 11:42:43 -0600

    Added local branch environment.
    - Necessary for computing branch information for the local environment.

---
+++ lib/git/cop/kit/environments/local.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "open3"
+
+module Git
+  module Cop
+    module Kit
+      module Environments
+        # Provides branch information for local development environment.
+        class Local
+          def initialize shell: Open3
+            @shell = shell
+          end
+
+          def name
+            result, _status = shell.capture2e "git rev-parse --abbrev-ref HEAD | tr -d '\n'"
+            result
+          end
+
+          def shas
+            result, _status = shell.capture2e %(git log --pretty=format:"%H" master..#{name})
+            result.split("\n")
+          end
+
+          private
+
+          attr_reader :shell
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/git/cop/kit/environments/local_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Git::Cop::Kit::Environments::Local do
+  let(:shell) { class_spy Open3 }
+  subject { described_class.new shell: shell }
+
+  describe "#name" do
+    it "answers Git branch name" do
+      command = "git rev-parse --abbrev-ref HEAD | tr -d '\n'"
+      allow(shell).to receive(:capture2e).with(command).and_return(["test", true])
+
+      expect(subject.name).to eq("test")
+    end
+  end
+
+  describe "#shas" do
+    it "answers Git commit SHAs" do
+      command = %(git log --pretty=format:"%H" master..test)
+      allow(subject).to receive(:name).and_return("test")
+      allow(shell).to receive(:capture2e).with(command).and_return(["abc\ndef", true])
+
+      expect(subject.shas).to contain_exactly("abc", "def")
+    end
+  end
+end
