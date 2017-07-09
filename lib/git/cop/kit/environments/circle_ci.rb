d740ed91cac522e1de277b297f0cfd04048c37d3
Brooke Kuhlmann <brooke@alchemists.io>
2017-07-09 11:45:33 -0600

    Added Circle CI branch environment.
    - Necessary for computing branch information for the
      [Circle CI](https://circleci.com) build environment.

---
+++ lib/git/cop/kit/environments/circle_ci.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "open3"
+
+module Git
+  module Cop
+    module Kit
+      module Environments
+        # Provides branch information for Circle CI build environment.
+        class CircleCI
+          def initialize shell: Open3
+            @shell = shell
+          end
+
+          def name
+            result, _status = shell.capture2e "git rev-parse --abbrev-ref HEAD | tr -d '\n'"
+            "origin/#{result}"
+          end
+
+          def shas
+            result, _status = shell.capture2e %(git log --pretty=format:"%H" origin/master..#{name})
+            result.split("\n")
+          end
+
+          private
+
+          attr_reader :shell
+        end
+      end
+    end
+  end
+end
