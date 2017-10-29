7b7360ffd41c48cbb53c82fe77667b8846f6ffbb
Brooke Kuhlmann <brooke@alchemists.io>
2017-10-29 15:08:00 -0600

    Updated to Rubocop 0.51.0.
    [Changes](http://bit.ly/2hlepWS).
    
    There is a bug with Rubocop 0.51.0 where the `Style/CommentedKeyword`
    cop raises false positives but can be ignored until the next release of
    Rubocop is issued.

--- lib/git/cop/errors/base.rb
+++ lib/git/cop/errors/base.rb
@@ -4,6 +4,7 @@ module Git
   module Cop
     module Errors
       # The root class of gem related errors.
+      # rubocop:disable Style/CommentedKeyword
       class Base < StandardError
         def initialize message = "Invalid #{Identity.label} action."
           super message
